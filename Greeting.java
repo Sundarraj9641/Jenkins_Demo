8233e9b381dfd1111886811021ce5f77d97d41cd
Sundarraj9641 <111488540+Sundarraj9641@users.noreply.github.com>
2023-08-31 11:51:52 +0530

    edited

--- Greeting.java
+++ Greeting.java
@@ -2,6 +2,6 @@ public class Greeting{
 	public static void main(String[] args){
 		System.out.println("Hello Sundarraj");
 		System.out.println("Welcome!!!!");
-		System.out.println("How are you");
+		System.out.println("How can i help you");
 	}
 }
